44fbeb9931a616727b3a628c64eb96df51b3bb9d
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-07-22 01:00:11 +0300

    Update unga.php

--- uhakiki/unga.php
+++ uhakiki/unga.php
@@ -1,6 +1,7 @@
 <?php
-$unga = mysqli_connect('localhost', 'root', 'root', 'comics');
+$unga = mysqli_connect('dpg-cqent4g8fa8c73ebkjb0-a', 'comics_zmjk_user', 's74Cuj7sI19DzDK7yu1kxYxRvl8IRkYe', 'comics_zmjk');
 
 if (!$unga) {
     die("No database connection <br>" . mysqli_connect_error());
 }
+?>
