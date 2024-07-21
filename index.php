696e952e417a7850b6cdf3008fd8677f3a4dce73
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-07-21 22:22:29 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['uid'])) {
+    header("location: login.php");
+    exit;
+} else {
+    header("location: home.php");
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COMICS NET</title>
+</head>
+<body>
+    Backend by <a href="https://samochu.com">SAM OCHU</a>
+</body>
+</html>
