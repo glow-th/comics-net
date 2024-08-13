a5989b3bd94c396bd1ccd46eb94d43219273625d
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-08-13 18:23:00 +0300

    Update login.php

--- uhakiki/login.php
+++ uhakiki/login.php
@@ -34,10 +34,3 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 }
 ?>
-
-<!-- Login Form -->
-<form method="POST" action="">
-    <input type="email" name="email" placeholder="Email" required><br>
-    <input type="password" name="password" placeholder="Password" required><br>
-    <button type="submit">Login</button>
-</form>
